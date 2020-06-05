a53fb7dffad97b25284c32eeb1d6cad73ac99778
Vincent Newsom <vnewsom90@gmail.com>
2020-06-05 14:25:06 -0400

    resized default profile pic

--- src/components/user-profile/UserProfile.js
+++ src/components/user-profile/UserProfile.js
@@ -20,22 +20,22 @@ export const UserProfile = (props) => {
   return (
     <Grommet theme={grommet}>
       <Main margin="medium" pad="small">
-      This is {bio !== undefined && bio.user.displayName}'s User Profile
-      {bio !== undefined && (
-        <div>
-          <h3>UserName: {bio.user.username}</h3>
-          {/* <img src={bio.user.pictureLocation} alt="profile picture" /> */}
-          <img 
-            src={BlankProfile} 
-            alt="profile picture" 
-          />
-          <p>About: {bio.user.about}</p>
-          <p>Last Update: {bio.user.updatedAt}</p>
-          <p>Created Profile: {bio.user.createdAt}</p>
-        </div>
-      )}
-       </Main>
+        This is {bio !== undefined && bio.user.displayName}'s User Profile
+        {bio !== undefined && (
+          <div>
+            <h3>UserName: {bio.user.username}</h3>
+            {/* <img src={bio.user.pictureLocation} alt="profile picture" /> */}
+            <img
+              style={{ width: "200px", height: "200px" }}
+              src={BlankProfile}
+              alt="profile picture"
+            />
+            <p>About: {bio.user.about}</p>
+            <p>Last Update: {bio.user.updatedAt}</p>
+            <p>Created Profile: {bio.user.createdAt}</p>
+          </div>
+        )}
+      </Main>
     </Grommet>
   );
 };
-
