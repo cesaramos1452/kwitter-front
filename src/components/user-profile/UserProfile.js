a01ee54f3bd805759db7339a4bba49aadac4c48c
Vincent Newsom <vnewsom90@gmail.com>
2020-06-04 23:14:45 -0400

    makes UserProfile dynamic rendering userInfo pass passes username props through Profile.js to UserProfile I am considering changing the name of this component to userProfileCard to be able to reuse component later

--- src/components/user-profile/UserProfile.js
+++ src/components/user-profile/UserProfile.js
@@ -2,13 +2,28 @@ import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 
-export const UserProfile = () => {
-  const [bio, setbio] = useState("vinchinzo123");
+export const UserProfile = (props) => {
+  const [bio, setbio] = useState(undefined);
 
   useEffect(() => {
-    fetch("http://kwitter-api.herokuapp.com/users/vinchinzo123")
+    fetch(`http://kwitter-api.herokuapp.com/users/${props.username}`)
       .then((res) => res.json())
       .then((user) => setbio(user));
-  }, [0]);
-  return <div>This is the User Profile {console.log(bio.user)}</div>;
+  }, [props.username]);
+  console.log(bio);
+  console.log({ props });
+  return (
+    <div>
+      This is {bio !== undefined && bio.user.displayName}'s User Profile
+      {bio !== undefined && (
+        <div>
+          <h3>UserName: {bio.user.username}</h3>
+          <img src={bio.user.pictureLocation} alt="profile picture" />
+          <p>About: {bio.user.about}</p>
+          <p>Last Update: {bio.user.updatedAt}</p>
+          <p>Created Profile: {bio.user.createdAt}</p>
+        </div>
+      )}
+    </div>
+  );
 };
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -2,10 +2,15 @@ import React from "react";
 import { MenuContainer } from "../components";
 import { UserProfile } from "../components";
 
-export const ProfileScreen = () => (
-  <>
-    <MenuContainer />
-    <h2>Profile</h2>
-    <UserProfile />
-  </>
-);
+export const ProfileScreen = (props) => {
+  console.log(props);
+  const { username } = props.match.params;
+  console.log(username);
+  return (
+    <>
+      <MenuContainer />
+      <h2>Profile</h2>
+      <UserProfile username={username} />
+    </>
+  );
+};
