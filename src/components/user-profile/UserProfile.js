0e1fe1d08b96aff2350f7b4ae0e3ac2b51c9f373
Veronica Fuentes <veronica.elsa1021@gmail.com>
2020-06-05 13:01:01 -0500

    css for profile

--- src/components/user-profile/UserProfile.js
+++ src/components/user-profile/UserProfile.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
+import BlankProfile from "./blank-profile.png";
+
+import { Grommet, Header, Main, Text } from "grommet";
+import { grommet } from "grommet/themes";
+import "./UserProfile.css";
 
 export const UserProfile = (props) => {
   const [bio, setbio] = useState(undefined);
@@ -13,17 +18,24 @@ export const UserProfile = (props) => {
   console.log(bio);
   console.log({ props });
   return (
-    <div>
+    <Grommet theme={grommet}>
+      <Main margin="medium" pad="small">
       This is {bio !== undefined && bio.user.displayName}'s User Profile
       {bio !== undefined && (
         <div>
           <h3>UserName: {bio.user.username}</h3>
-          <img src={bio.user.pictureLocation} alt="profile picture" />
+          {/* <img src={bio.user.pictureLocation} alt="profile picture" /> */}
+          <img 
+            src={BlankProfile} 
+            alt="profile picture" 
+          />
           <p>About: {bio.user.about}</p>
           <p>Last Update: {bio.user.updatedAt}</p>
           <p>Created Profile: {bio.user.createdAt}</p>
         </div>
       )}
-    </div>
+       </Main>
+    </Grommet>
   );
 };
+
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -2,15 +2,25 @@ import React from "react";
 import { MenuContainer } from "../components";
 import { UserProfile } from "../components";
 
+import { Grommet, Header, Main, Text } from "grommet";
+import { grommet } from "grommet/themes";
+
 export const ProfileScreen = (props) => {
   console.log(props);
   const { username } = props.match.params;
   console.log(username);
   return (
-    <>
+    <Grommet theme={grommet}>
       <MenuContainer />
-      <h2>Profile</h2>
+      <Main margin="medium" pad="small">
+        <h2>Profile</h2>
+      </Main>
       <UserProfile username={username} />
-    </>
+    </Grommet>
   );
 };
+
+
+
+      
+    
