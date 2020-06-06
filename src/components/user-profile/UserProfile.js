da5c955803de023b09ac03b74a6454b61b42d571
Vincent Newsom <vnewsom90@gmail.com>
2020-06-06 16:30:22 -0400

    add update user toggle function and TODO list

--- src/components/user-profile/UserProfile.js
+++ src/components/user-profile/UserProfile.js
@@ -3,18 +3,23 @@ import { useState } from "react";
 import { useEffect } from "react";
 import BlankProfile from "./blank-profile.png";
 
-import { Grommet, Header, Main, Text } from "grommet";
+import { Grommet, Header, Main, Text, Button } from "grommet";
 import { grommet } from "grommet/themes";
 import "./UserProfile.css";
 
 export const UserProfile = (props) => {
   const [bio, setbio] = useState(undefined);
+  const [updating, setUpdating] = useState(false);
 
   useEffect(() => {
     fetch(`http://kwitter-api.herokuapp.com/users/${props.username}`)
       .then((res) => res.json())
       .then((user) => setbio(user));
   }, [props.username]);
+
+  const updateProfile = () => {
+    setUpdating(!updating);
+  };
   console.log(bio);
   console.log({ props });
   return (
@@ -27,12 +32,16 @@ export const UserProfile = (props) => {
             {/* <img src={bio.user.pictureLocation} alt="profile picture" /> */}
             <img
               style={{ width: "200px", height: "200px" }}
-              src={BlankProfile}
+              src={bio.user.pictureLocation || BlankProfile}
               alt="profile picture"
             />
             <p>About: {bio.user.about}</p>
             <p>Last Update: {bio.user.updatedAt}</p>
             <p>Created Profile: {bio.user.createdAt}</p>
+            {/* TODO: conditional render if updating is true or false */}
+            {/* add SAVE BUTTON, andd DISCARD button */}
+            {/* Have input fields to update prefilled with from API call */}
+            <button onClick={updateProfile}>Update Bio</button>
           </div>
         )}
       </Main>
