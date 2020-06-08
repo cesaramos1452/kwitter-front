4b25663d950254836541662522c7db60e5a208db
Veronica Fuentes <veronica.elsa1021@gmail.com>
2020-06-08 11:44:24 -0500

    fixing bug infinite spinner

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { css } from "@emotion/core";
-
 import CircleLoader from "react-spinners/CircleLoader";
 
 /*
@@ -8,6 +7,14 @@ https://lmgtfy.com/?q=react+loading+spinner+components
 There are many more on the web use one of these or create your own!
 */
 
-export const Loader = () => <><CircleLoader className="loader" /></>;
 
-    
+export const Loader = () => <>
+                                <CircleLoader 
+                                    css={css`
+                                    display: block;
+                                    margin: 0 auto;
+                                  `}
+                                    color={"#735f9c"}  
+                                />
+                            </>;
+
