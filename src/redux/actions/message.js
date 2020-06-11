27c52c6b02166e423729437021f179e413ba50bc
Cesar Ramos <cesaramos1452@yahoo.com>
2020-06-10 20:24:50 -0700

    BEGAN ON ACTION MESSAGES MAKING ACTION CREATORS

--- src/redux/actions/message.js
+++ src/redux/actions/message.js
@@ -1,2 +1,6 @@
 import api from "../../utils/api";
 
+export const GET_LIST_MESSAGES = 'GET_LIST_MESSAGES';
+export const CREATE_MESSAGE = 'CREATE_MESSAGE';
+export const GET_MESSAGE = 'GET_MESSAGE';
+export const DELETE_MESSAGE = 'DELETE_MESSAGE';
