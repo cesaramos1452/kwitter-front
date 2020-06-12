9be90265ad6b6898c3d46f6f1a1e6f5189d1bd09
Vincent Newsom <vnewsom90@gmail.com>
2020-06-12 18:10:40 -0400

    adds flex to the updateprofile options

--- src/components/user-profile-update-form/UserProfileUpdateForm.js
+++ src/components/user-profile-update-form/UserProfileUpdateForm.js
@@ -66,94 +66,98 @@ export const UserProfileUpdateForm = (props) => {
   return (
     <Grommet full theme={grommet}>
       <Box fill align="baseline">
-        <img
-          className="ProfileImg"
-          src={
-            props.users !== undefined
-              ? "https://kwitter-api.herokuapp.com" + picture
-              : BlankProfile
-          }
-          alt={picture}
-        />
-        <form onSubmit={handleSubmit}>
-          <label>
-            <TextInput type="file" ref={fileInput} name="picture" />
-          </label>
-          <br />
-          <Button type="submit" label="Upload" primary />
-        </form>
-        <Form className="updateForm" onSubmit={clickHandler}>
-          <Box
-            className="textField"
-            direction="row"
-            align="center"
-            round="small"
-            border
-          >
-            <TextInput
-              type="text"
-              name="displayName"
-              value={input.displayName}
-              onChange={changeHandler}
-              placeholder={props.users.displayName}
-            />
-          </Box>
-          <Box
-            className="textField"
-            direction="row"
-            align="center"
-            round="small"
-            border
-          >
-            <TextInput
-              type="text"
-              name="about"
-              value={input.about}
-              onChange={changeHandler}
-              placeholder={
-                props.users.about === "" ? "add a bio" : props.users.about
+        <div className="updateProfile" style={{ display: "flex" }}>
+          <div className="updatePhoto">
+            <img
+              className="ProfileImg"
+              src={
+                props.users !== undefined
+                  ? "https://kwitter-api.herokuapp.com" + picture
+                  : BlankProfile
               }
+              alt={picture}
             />
-          </Box>
-          <Box
-            className="textField"
-            direction="row"
-            align="center"
-            round="small"
-            border
-          >
-            <TextInput
-              type={reveal ? "text" : "password"}
-              name="password"
-              required
-              value={input.password}
-              onChange={changeHandler}
-              placeholder="enter password to commit changes"
-            />
-            <Button
-              icon={reveal ? <View size="medium" /> : <Hide size="medium" />}
-              onClick={() => setReveal(!reveal)}
-            />
-          </Box>
-          <Box direction="row" justify="between" margin={{ top: "medium" }}>
-            <Link to={`/profiles/${props.username}`}>
-              <Button
-                label="Save Changes"
-                primary
-                type="submit"
-                onClick={clickHandler}
+            <form onSubmit={handleSubmit}>
+              <label>
+                <TextInput type="file" ref={fileInput} name="picture" />
+              </label>
+              <br />
+              <Button type="submit" label="Upload" primary />
+            </form>
+          </div>
+          <Form className="updateForm" onSubmit={clickHandler}>
+            <Box
+              className="textField"
+              direction="row"
+              align="center"
+              round="small"
+              border
+            >
+              <TextInput
+                type="text"
+                name="displayName"
+                value={input.displayName}
+                onChange={changeHandler}
+                placeholder={props.users.displayName}
               />
-            </Link>
-            <Link to={`/`}>
-              <Button
-                secondary
-                label="DELETE ACCOUNT"
-                onClick={handleDeleteUser}
-                color="red"
+            </Box>
+            <Box
+              className="textField"
+              direction="row"
+              align="center"
+              round="small"
+              border
+            >
+              <TextInput
+                type="text"
+                name="about"
+                value={input.about}
+                onChange={changeHandler}
+                placeholder={
+                  props.users.about === "" ? "add a bio" : props.users.about
+                }
               />
-            </Link>
-          </Box>
-        </Form>
+            </Box>
+            <Box
+              className="textField"
+              direction="row"
+              align="center"
+              round="small"
+              border
+            >
+              <TextInput
+                type={reveal ? "text" : "password"}
+                name="password"
+                required
+                value={input.password}
+                onChange={changeHandler}
+                placeholder="enter password to commit changes"
+              />
+              <Button
+                icon={reveal ? <View size="medium" /> : <Hide size="medium" />}
+                onClick={() => setReveal(!reveal)}
+              />
+            </Box>
+            <Box direction="row" justify="between" margin={{ top: "medium" }}>
+              <Link to={`/profiles/${props.username}`}>
+                <Button
+                  label="Save Changes"
+                  primary
+                  type="submit"
+                  onClick={clickHandler}
+                />
+              </Link>
+              <Link to={`/`}>
+                <Button
+                  secondary
+                  label="DELETE ACCOUNT"
+                  onClick={handleDeleteUser}
+                  color="red"
+                />
+              </Link>
+            </Box>
+          </Form>
+        </div>
       </Box>
     </Grommet>
   );
