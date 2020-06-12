a87caa2d3d21bed6727f1b6b5f94ead56d2f7ffb
veronica fuentes <veronica.elsa1021@gmail.com>
2020-06-11 20:02:31 -0500

    the css for the messages

--- src/components/messages/Messages.js
+++ src/components/messages/Messages.js
@@ -1,19 +1,26 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./Messages.css";
+
+import { Avatar, Box, Grommet } from "grommet";
+import { grommet } from "grommet/themes";
 
 const Messages = (props) => {
   return (
-    <div>
+    <Grommet theme={grommet}>
+        <Box border className="messagesList">
       {props.messsages !== [] &&
         props.messages.map((message) => (
           <>
-            <li style={{ listStyle: "none" }}>
-              message text{message.text} username {message.username}
+            <li className="message">
+            <Avatar className="AvatarImg" src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80" />
+            {message.username}
+            {message.text} 
             </li>
           </>
         ))}
       <textarea rows="4" cols="20" />
-    </div>
+      </Box>
+    </Grommet>
   );
 };
 
