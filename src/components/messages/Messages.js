031bfaf442b216bee67ff5ae20d45832cf45febe
Vincent Newsom <vnewsom90@gmail.com>
2020-06-12 18:10:40 -0400

    remove candypost

--- src/components/messages/Messages.js
+++ src/components/messages/Messages.js
@@ -6,23 +6,48 @@ import { grommet } from "grommet/themes";
 
 export const Messages = (props) => {
   console.log({ props });
+  const determinePictureFound = (username) => {
+    let result = fetch(
+      "https://kwitter-api.herokuapp.com/users/" + username + "/picture"
+    ).then((response) => {
+      console.log(response);
+      if (response.ok) {
+        return (
+          "https://kwitter-api.herokuapp.com/users/" + username + "/picture"
+        );
+      } else {
+        return "//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80";
+      }
+    });
+    console.log(result);
+    return result;
+  };
+
   useEffect(() => {
     props.getMessagesList();
   }, []);
 
   return (
     <Grommet theme={grommet}>
-      <Box border pad="medium" elevation="medium" className="messagesList" style={{maxWidth:"710px"} } >
+      <Box
+        border
+        pad="medium"
+        elevation="medium"
+        className="messagesList"
+        style={{ maxWidth: "710px" }}
+      >
         {props.messsages !== [] &&
           props.messages.map((message) => (
             <Box border pad="medium" elevation="medium" className="message">
-            <Avatar className="AvatarImg" src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80" />
-            {message.username}
-            {message.text} 
+              <Avatar
+                className="AvatarImg"
+                src={determinePictureFound(message.username)}
+              />
+              {message.username}
+              {message.text}
             </Box>
-        ))}
-      <textarea rows="4" cols="20" />
-      
+          ))}
+        <textarea rows="4" cols="20" />
       </Box>
     </Grommet>
   );
