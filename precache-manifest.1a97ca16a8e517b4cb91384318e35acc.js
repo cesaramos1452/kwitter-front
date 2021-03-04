3201f6be54eb042cdb9daebf1fb4f163a34906e7
Cesar Ramos <cesaramos1452@yahoo.com>
2021-03-04 00:17:57 -0800

    Updates

---
+++ precache-manifest.1a97ca16a8e517b4cb91384318e35acc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a846802ec2a9dfbf99357dce2c9df35",
+    "url": "/kwitter-frontend/index.html"
+  },
+  {
+    "revision": "6dc011a78b3f2614eecc",
+    "url": "/kwitter-frontend/static/css/main.2d1b6a8f.chunk.css"
+  },
+  {
+    "revision": "8bb942bd4e05e129b6cb",
+    "url": "/kwitter-frontend/static/js/2.6ce1236a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kwitter-frontend/static/js/2.6ce1236a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dc011a78b3f2614eecc",
+    "url": "/kwitter-frontend/static/js/main.30a72fed.chunk.js"
+  },
+  {
+    "revision": "9d3df622bc47b28f04db",
+    "url": "/kwitter-frontend/static/js/runtime-main.14d0a5c8.js"
+  },
+  {
+    "revision": "917639f2213e4d602ba02559c4d09eab",
+    "url": "/kwitter-frontend/static/media/candyBear.917639f2.png"
+  },
+  {
+    "revision": "6a4231816acb3891ed46d4e5a35945ec",
+    "url": "/kwitter-frontend/static/media/dislikeCandy.6a423181.png"
+  },
+  {
+    "revision": "39e166a6f498cba55a52c1218ed5ddf5",
+    "url": "/kwitter-frontend/static/media/likeCandy.39e166a6.png"
+  },
+  {
+    "revision": "88ade12357db9433c25b5a9adf9a61a9",
+    "url": "/kwitter-frontend/static/media/logo.88ade123.png"
+  }
+]);
